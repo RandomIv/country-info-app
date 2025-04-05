15d292f3b6bd9c5c096ce0db5ff60275f12535ee
RandomIv <ivanishin.alexander@lll.kpi.ua>
2025-04-05 16:40:03 +0300

    feat: set up environment variables -add .env file(in real projects commiting .env file not recommended) -make ConfigureModule global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CountryModule } from './country/country.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [CountryModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
